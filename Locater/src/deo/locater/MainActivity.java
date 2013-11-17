b38bbc95e5ca6555872504d53aa8e5b1f6197247
Andrew Branch <abranch6@gatech.edu>
2013-11-16 20:08:28 -0500

    Added onPause and onResume for saving states

--- Locater/src/deo/locater/MainActivity.java
+++ Locater/src/deo/locater/MainActivity.java
@@ -2,7 +2,9 @@ package deo.locater;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,29 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@Override
+	protected void onPause() 
+	{
+	    super.onPause();
+	    SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+	    SharedPreferences.Editor editor = preferences.edit();  // Put the values from the UI
+
+	    editor.putFloat("Latitude", (float)0.0);
+	    editor.putFloat("Longitude", (float)0.0);
+	    editor.putBoolean("Location Stored", true);    
+	    editor.commit();
+	}
+	
+	@Override
+    protected void onResume() 
+    {
+        super.onResume();
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        
+        preferences.getBoolean("Location Stored", false);
+        preferences.getFloat("Latitude", (float)0.0);
+        preferences.getFloat("Longitude", (float)0.0);
+    }
 
 }
